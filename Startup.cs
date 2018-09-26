b44b0dc74afc35feea48aa11f06ac7bf40d07382
sajithmanuel <sajithmanuel@SAJITHLEYA>
2018-09-26 18:13:48 -0300

    Git hub Profile page

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+
+
 namespace ProfilePage
 {
     public class Startup
